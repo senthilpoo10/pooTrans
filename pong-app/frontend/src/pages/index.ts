ecad0dd916896d26a6d7978d62c0b14eea5cf797
senthilpoo10 <senthilpoo10@gmail.com>
2025-09-20 00:23:14 +0300

    Delete pong-app/frontend/src directory

--- pong-app/frontend/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-// frontend/src/pages/index.ts
-export * as authorised from "./authorised";
-export * as unauthorised from "./unauthorised";
-export * as game from "./game";
-export * as general from "./general";
